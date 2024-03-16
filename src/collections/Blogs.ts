a057fa849f73078be50bd38a9c3b905516d87a8a
shahadat742380 <shahadat.peacockindia@gmail.com>
2024-03-16 10:09:28 +0600

    Create CURD opration

--- src/collections/Blogs.ts
+++ src/collections/Blogs.ts
@@ -2,28 +2,54 @@ import { CollectionConfig } from "payload/types";
 
 const Blogs: CollectionConfig = {
   slug: "blogs",
-  // auth: true,
-  // admin: {
-  //   useAsTitle: 'email',
-  // },
+  upload: {
+    staticURL: "/images/blogs",
+    staticDir: "/images/blogs",
+    imageSizes: [
+      {
+        name: "thumbnail",
+        width: 400,
+        height: 300,
+        position: "centre",
+      },
+      {
+        name: "card",
+        width: 768,
+        height: 1024,
+        position: "centre",
+      },
+      {
+        name: "tablet",
+        width: 1024,
+        // By specifying `undefined` or leaving a height undefined,
+        // the image will be sized to a certain width,
+        // but it will retain its original aspect ratio
+        // and calculate a height automatically.
+        height: undefined,
+        position: "centre",
+      },
+    ],
+    adminThumbnail: "thumbnail",
+    mimeTypes: ["image/*"],
+  },
   fields: [
     {
       name: "author",
       type: "text",
-      required: true
+      required: true,
     },
     {
-      name: "author-title",
+      name: "authorTitle",
       type: "text",
-      required: true
+      required: true,
     },
     {
-      name: "blog-title",
+      name: "blogTitle",
       type: "text",
-      required: true
+      required: true,
     },
     {
-      name: "blog-des",
+      name: "blogDes",
       type: "text",
       required: true,
     },
